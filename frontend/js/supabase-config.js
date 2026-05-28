a964fe39cf2d75ff67286d0861c68c0ffa5db84b
stebancito <192398777+stebancito@users.noreply.github.com>
2026-05-28 15:05:39 -0600

    Update supabase-config.js

--- frontend/js/supabase-config.js
+++ frontend/js/supabase-config.js
@@ -1,5 +1,7 @@
 const SUPABASE_URL = 'https://rsrovjfvjnhhmfnqwsws.supabase.co';   // URL del proyecto
-const SUPABASE_ANON_KEY = '******';                     // Clave anónima
-
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzcm92amZ2am5oaG1mbnF3c3dzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4OTgwODQsImV4cCI6MjA5NTQ3NDA4NH0._54cSp_uBRS7ThaRu9k9NvZ92NIV_Rk7GepNN3HX-cA';                     // Clave anónima
+// Crear el cliente de Supabase (una sola vez)
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Exponerlo globalmente para que otros scripts lo usen
 window.supabaseClient = supabaseClient;
